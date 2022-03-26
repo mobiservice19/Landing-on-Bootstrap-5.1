3b498e271c21ca8010c0779026dd2640545cd842
mobiservice19 <mobiservice19@gmail.com>
2022-03-26 19:08:36 +0300

    New_Installation

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+?>
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Обязательные метатеги -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Beautiful snippet" />
+    <meta name="keywords" content="gyro-toys.ru" />
+    <!-- Подключаем Bootstrap CSS -->
+    <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="/css/images/icon/favicon.png">
+    <title>GYRO-TOYS.RU</title>
+  </head>
+  <body>
+    <!-- Подключаем Bootstrap в связке с Popper -->    
+    <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+    <div class='content'>Feadback
+    <div class='text'>info@gyro-toys.ru</div> </div>
+
+
+   </body>
+</html>
