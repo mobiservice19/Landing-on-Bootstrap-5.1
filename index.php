08ac6dd3cb9fc25e362752c93687a6a2b93d8bd8
mobiservice19 <mobiservice19@gmail.com>
2022-03-27 02:00:14 +0300

    v. 0.0.1

--- index.php
+++ index.php
@@ -3,25 +3,27 @@ require __DIR__.'/vendor/autoload.php';
 ?>
 <!doctype html>
 <html lang="ru">
-  <head>
-    <!-- Обязательные метатеги -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Beautiful snippet" />
-    <meta name="keywords" content="gyro-toys.ru" />
-    <!-- Подключаем Bootstrap CSS -->
-    <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="icon" type="image/png" sizes="16x16" href="/css/images/icon/favicon.png">
-    <title>GYRO-TOYS.RU</title>
-  </head>
-  <body>
-    <!-- Подключаем Bootstrap в связке с Popper -->    
-    <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+	<head>
+		<!-- Обязательные метатеги -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="Beautiful snippet" />
+		<meta name="keywords" content="gyro-toys.ru" />
+		<!-- Подключаем Bootstrap CSS -->
+		<link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="/css/style.css">
+		<link rel="icon" type="image/png" sizes="16x16" href="/css/images/icon/favicon.png">
+		<title>GYRO-TOYS.RU</title>
 
-    <div class='content'>Feadback
-    <div class='text'>info@gyro-toys.ru</div> </div>
+	</head>
+	<body>
+		<!-- Подключаем Bootstrap в связке с Popper -->    
+		<script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 
-   </body>
+		
+		<div class='content'>Feadback
+				<div class='text'>info@gyro-toys.ru</div> 
+		</div>
+	 </body>
 </html>
