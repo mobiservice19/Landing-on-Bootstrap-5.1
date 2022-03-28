03861bfbd26e1ba792da946469374ec4a88fe547
mobiservice19 <mobiservice19@gmail.com>
2022-03-28 11:25:55 +0300

    v.0.0.4

--- index.php
+++ index.php
@@ -4,27 +4,26 @@ require __DIR__.'/vendor/autoload.php';
 <!doctype html>
 <html lang="ru">
 	<head>
-		<!-- Обязательные метатеги -->
+		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="description" content="Beautiful snippet" />
-		<meta name="keywords" content="gyro-toys.ru" />
-		<!-- Подключаем Bootstrap CSS -->
+		<meta name="keywords" content="gyro-toys.ru, GYRO, gyro, TOYS, toys, gyro toys" />
+		<!-- Connect Bootstrap CSS -->
 		<link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" />
+		<!-- Connect Style CSS -->
 		<link rel="stylesheet" href="/css/style.css">
+		<!-- ICON for web site -->
 		<link rel="icon" type="image/png" sizes="16x16" href="/css/images/icon/favicon.png">
+		<!-- Web site name -->
 		<title>GYRO-TOYS.RU</title>
-
 	</head>
 	<body>
-		<!-- Подключаем Bootstrap в связке с Popper -->    
-		<script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-
-
-		
 		<div class='content'>Feadback
 				<div class='text'>info@gyro-toys.ru</div> 
 		</div>
+		<!-- Connect Bootstrap + Popper -->    
+		<script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 		<!-- Yandex.Metrika counter -->
 			<script type="text/javascript" >
@@ -38,7 +37,7 @@ require __DIR__.'/vendor/autoload.php';
 				accurateTrackBounce:true
 			});
 			</script>
-			<noscript><div><img src="https://mc.yandex.ru/watch/56993626" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+			<noscript><div><img src="https://mc.yandex.ru/watch/56993626" style="position:absolute; left:-9999px;" alt="Yandex.Metrika" /></div></noscript>
 		<!-- /Yandex.Metrika counter -->
 	 </body>
 </html>
